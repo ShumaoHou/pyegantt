a4e98d2c42c5b63024e550b7c646d1b43d80e402
Shumao Hou <910536551@qq.com>
2021-04-24 16:57:16 +0800

    解决符号输出变为转义符号的问题；直接生成页面文件；
    TODO：
    1、自定义接口，替换数据。

--- html/html_creator.py
+++ html/html_creator.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 import dominate
 from dominate.tags import *
 
-doc = dominate.document(title='Dominate your HTML')
 
-with doc.head:
-    script(src='http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js')
-    script(src='https://cdn.jsdelivr.net/npm/echarts@5.1.0/dist/echarts.min.js')
+def create_html(html_file_path='pyegantt.html'):
+    doc = dominate.document(title='pyegantt')
 
-js = open('../js/echart_gantt.js', 'r', encoding='UTF-8').read()
+    with doc.head:
+        script(src='http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js')
+        script(src='https://cdn.jsdelivr.net/npm/echarts@5.1.0/dist/echarts.min.js')
 
-with doc:
-    with div():
-        attr(id='main', style='width: 1600px; height:800px;')
-    with script(js):
-        attr(type='text/javascript')
+    with open('../js/echart_gantt.js', 'r', encoding='UTF-8') as f:
+        js = f.read()
 
-print(doc)
+    with doc:
+        with div():
+            attr(id='main', style='width: 1600px; height:800px;')
+        with script(js):
+            attr(type='text/javascript')
+
+    # 去掉转义字符串
+    doc_str = str(doc)
+    doc_str = doc_str.replace('&amp;', '&')
+    doc_str = doc_str.replace('&lt;', '<')
+    doc_str = doc_str.replace('&gt;', '>')
+
+    with open(html_file_path, 'w') as f:
+        f.write(doc_str)
+
+    os.startfile(html_file_path)
+
+
+if __name__ == '__main__':
+    create_html()
